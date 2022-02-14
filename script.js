71f97e6399bafe6a73d89b945c065350151de73a
si-shakil <sishakil850@gmail.com>
2022-02-14 22:44:01 +0600

    last commit

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ function digitalClock() {
     let minutes = time.getMinutes();
     let seconds = time.getSeconds();
     let timeFormat = 'AM';
+    if (hours === 0) {
+        hours = 12;
+    }
+    if (hours > 12) {
+        hours = hours - 12;
+        timeFormat = 'PM';
+    }
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
 
-    let finalTime = `${hours}:${minutes}:${seconds}`
+    let finalTime = `${hours}:${minutes}:${seconds}`;
 
     document.getElementById('time').innerText = finalTime;
     document.getElementById('ampm').innerText = timeFormat;
