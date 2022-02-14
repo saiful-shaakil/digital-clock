da3e150655429d4e1e5607588e9820738c3d304d
si-shakil <sishakil850@gmail.com>
2022-02-14 22:35:53 +0600

    fourth commit

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ function digitalClock() {
     let hours = time.getHours();
     let minutes = time.getMinutes();
     let seconds = time.getSeconds();
+    let timeFormat = 'AM';
+
+    let finalTime = `${hours}:${minutes}:${seconds}`
+
+    document.getElementById('time').innerText = finalTime;
+    document.getElementById('ampm').innerText = timeFormat;
+    setInterval(digitalClock, 1000);
 }
+digitalClock();
